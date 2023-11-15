4dc9fbca24f7ff46c6cebe7ede57f3b620ebe46b
Connor <connor.cs@protonmail.com>
2023-11-14 20:34:40 -0500

    validation function complete

--- src/App.jsx
+++ src/App.jsx
@@ -6,34 +6,31 @@ import "./App.css";
 
 function App() {
   const [isValid, setIsValid] = useState(true);
+  //user input
   const [input, setInput] = useState({
-    day: "",
-    month: "",
-    year: "",
+    day: "1",
+    month: "7",
+    year: "2023",
   });
+  //results to be displayed after calculation
   const [results, setResults] = useState({
     days: "",
     months: "",
     years: "",
   });
+  //current date for comparison
   const [currentDate, setCurrentDate] = useState({
     day: "",
     month: "",
     year: "",
-  })
+  });
   const [errors, setErrors] = useState({
-    dayError: true,
+    dayError: false,
     monthError: false,
-    yearError: false
-  })
+    yearError: false,
+  });
 
-  // console.table(values);
-
-  // console.log(today);
-  // console.log(month);
-  // console.log(currentYear);
-  // console.log(todaysDate);
-
+  //get today's info on load
   useEffect(() => {
     const todaysDate = moment().format();
     const currentYear = todaysDate.slice(0, 4);
@@ -42,27 +39,57 @@ function App() {
     setCurrentDate({
       day: today,
       month: currentMonth,
-      year: currentYear
-    })
-  },[]);
+      year: currentYear,
+    });
+    // console.log('test',moment(currentMonth).daysInMonth())
+  }, []);
 
+  //is user input valid?
   const validate = () => {
+    const daysInMonth = moment(currentDate.month).daysInMonth();
     if (parseInt(currentDate.year) < parseInt(input.year)) {
-      setIsValid(false)
-      setErrors({...errors, yearError: true})
+      setIsValid(false);
+      setErrors({ ...errors, yearError: true });
+      console.log('bad year')
     }
-    if (parseInt(currentDate.month) > 12 && parseInt(currentDate.month) < 1) {
+    if (+input.month > 12 || +input.month < 1) {
+      setIsValid(false);
+      setErrors({ ...errors, monthError: true });
+      console.log('bad month')
+    }
+    if (!(+input.day >= 1) || !(+input.day <= daysInMonth)) {
       setIsValid(false)
-      setErrors({...errors, monthError: true})
+      setErrors({...errors, dayError: true})
+    } else console.log('okay');
+  };
+
+ 
+  
+  const submit = () => {
+    validate()
+    if (errors.dayError === false && errors.yearError=== false && errors.monthError===false) {
+      console.log('klkjlk')
     }
   };
 
   return (
     <>
       <div className="main">
-        <Input isValid={isValid} setInput={setInput} input={input} errors={errors}/>
+        <Input
+          isValid={isValid}
+          setInput={setInput}
+          input={input}
+          errors={errors}
+        />
         <hr></hr>
+
         <div className="display-section">
+          <img
+            onClick={() => submit()}
+            className="arrow"
+            src="/assets/images/icon-arrow.svg"
+            alt="arrow"
+          />
           <ul>
             <li>
               <span>--</span> years
